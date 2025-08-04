9b0e2c8d9c78554df95d2398c13d4ebc62c2e63f
tvlmedia <info@tvlmedia.nl>
2025-08-04 15:10:49 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const lenses = {
   "Cooke Panchro FF": "images/cooke_panchro.jpg",
   "DZO Vespid": "images/dzo_vespid.jpg",
@@ -14,7 +13,6 @@ const lensLeft = document.getElementById("lensLeft");
 const lensRight = document.getElementById("lensRight");
 const imageLeft = document.getElementById("imageLeft");
 const imageRight = document.getElementById("imageRight");
-const afterWrapper = document.getElementById("afterWrapper");
 const slider = document.getElementById("slider");
 const container = document.getElementById("compareContainer");
 
@@ -28,9 +26,8 @@ lensRight.selectedIndex = 1;
 imageLeft.src = lensLeft.value;
 imageRight.src = lensRight.value;
 
-// Zet startpositie slider & masker op 50%
-afterWrapper.style.width = "50%";
 slider.style.left = "50%";
+imageRight.style.clipPath = "inset(0 0 0 50%)";
 
 lensLeft.onchange = () => imageLeft.src = lensLeft.value;
 lensRight.onchange = () => imageRight.src = lensRight.value;
@@ -39,7 +36,7 @@ container.addEventListener("mousemove", (e) => {
   const rect = container.getBoundingClientRect();
   const x = e.clientX - rect.left;
   const percent = Math.max(0, Math.min(1, x / rect.width));
-  afterWrapper.style.width = `${percent * 100}%`;
+  imageRight.style.clipPath = `inset(0 0 0 ${percent * 100}%)`;
   slider.style.left = `${percent * 100}%`;
 });
 
@@ -47,6 +44,6 @@ container.addEventListener("touchmove", (e) => {
   const rect = container.getBoundingClientRect();
   const x = e.touches[0].clientX - rect.left;
   const percent = Math.max(0, Math.min(1, x / rect.width));
-  afterWrapper.style.width = `${percent * 100}%`;
+  imageRight.style.clipPath = `inset(0 0 0 ${percent * 100}%)`;
   slider.style.left = `${percent * 100}%`;
 });
