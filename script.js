c695b230ae199f65f4eef0eb075917d6fc3fe100
tvlmedia <info@tvlmedia.nl>
2025-08-05 01:22:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -208,7 +208,7 @@ document.getElementById("downloadPdfButton").addEventListener("click", async () 
   // Pagina 2: Left lens
   pdf.addPage();
   fillBlack();
-  const yLeftEnd = await drawImageCentered(leftData, 150, pageHeight - 350);
+  const yLeftEnd = await drawImageCentered(leftData, 150, pageHeight - 250);
   drawLogo(pageWidth - 100, 05, 80);
   pdf.setTextColor(255, 255, 255);
   pdf.setFontSize(14);
@@ -218,7 +218,7 @@ document.getElementById("downloadPdfButton").addEventListener("click", async () 
   // Pagina 3: Right lens
   pdf.addPage();
   fillBlack();
-  const yRightEnd = await drawImageCentered(rightData, 150, pageHeight - 350);
+  const yRightEnd = await drawImageCentered(rightData, 0, null, true);
   drawLogo(pageWidth - 100, 05, 80);
   pdf.setTextColor(255, 255, 255);
   pdf.setFontSize(14);
