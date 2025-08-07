ff4a1cb88994ff2434bcb442d79a715595076b10
tvlmedia <info@tvlmedia.nl>
2025-08-07 17:21:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const lenses = [
 const notes = {
   "ironglass_red_p_35mm": "37mm",
   "ironglass_zeiss_jena_35mm": "35mm",
-  "ironglass_red_p_58mm": "58mm",
+  "ironglass_red_p_50mm": "58mm", // Wordt getoond als 58mm
 "ironglass_zeiss_jena_50mm": "50mm"
   "cooke_panchro_ff_25mm": "32mm"
 };
@@ -20,7 +20,7 @@ const notes = {
 const lensImageMap = {
   "ironglass_red_p_35mm_t2_8": "red_p_37mm_t2_8.jpg",
   "ironglass_zeiss_jena_35mm_t2_8": "zeiss_jena_35mm_t2_8.jpg",
-  "ironglass_red_p_58mm_t2_8": "red_p_58mm_t2_8.jpg",
+  "ironglass_red_p_50mm_t2_8": "red_p_58mm_t2_8.jpg", // matcht op 50mm
 "ironglass_zeiss_jena_50mm_t2_8": "zeiss_jena_50mm_t2_8.jpg"
 };
 
