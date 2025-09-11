9883403be1251638b1263672753cb16e812685be
tvlmedia <info@tvlmedia.nl>
2025-09-11 23:18:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -378,7 +378,7 @@ const notes = {
   "ironglass_mkii_35mm": "37mm",
   "ironglass_zeiss_jena_50mm": "50mm",
   "cooke_panchro_ff_35mm": "32mm",
-  "cooke_panchro_ff_50mm": "50mm"
+  "cooke_panchro_ff_50mm": "50mm",
   "dzo_vespid_80mm": "75mm",
   "dzo_vespid_85mm": "75mm",
   "dzo_arles_80mm": "75mm",
